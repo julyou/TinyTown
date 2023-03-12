62b60dbcac58df77ada5204753429f90ba1d5914
ymnglli <ymnglli@gmail.com>
2023-03-12 06:21:24 -0700

    fixed character collision bounds

--- mask_loader.py
+++ mask_loader.py
@@ -2,10 +2,6 @@ from PIL import Image
 
 class MaskLoader:
     def __init__(self, path):
-        img = Image.open(path)
-<<<<<<< HEAD
-        self.pix = img.load()
-=======
-        self.pix = img.load()
-    
->>>>>>> ef2f2d49ffdd8ba19c23441f50c8083b22fd2b9a
+        self.img = Image.open(path)
+        self.pix = self.img.load()
+
