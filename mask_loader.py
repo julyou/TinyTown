ef2f2d49ffdd8ba19c23441f50c8083b22fd2b9a
Julia <juliayou604@gmail.com>
2023-03-12 01:56:21 -0800

    add collision detection

--- mask_loader.py
+++ mask_loader.py
@@ -4,6 +4,4 @@ class MaskLoader:
     def __init__(self, path):
         img = Image.open(path)
         self.pix = img.load()
-        self.width, self.height = img.width, img.height
-        self.mat = [[1] * self.width] * self.height
-
+    
