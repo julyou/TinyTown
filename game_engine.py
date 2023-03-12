0fcb20f4e28ad8af4ea12658af424f224d48bd40
Julia <juliayou604@gmail.com>
2023-03-11 21:10:11 -0800

    change background

--- game_engine.py
+++ game_engine.py
@@ -16,7 +16,7 @@ class GameEngine:
         self.next_frame = clock()
         self.loop = True
         self.messages = self.read_file("conversations.txt")
-        setBackgroundImage("graphics/junkyard2.png")
+        setBackgroundImage("graphics/background.png")
 
         speech = makeLabel("hi", 40, WIDTH*0.2, HEIGHT * 0.8, fontColour='white', font='TTF/dogicabold.ttf', background="clear")
         showLabel(speech)
